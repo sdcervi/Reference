831026b9a6aa88386bbccff9abd4fb6947d2e119
sdcervi <sdcervi@gmail.com>
2022-03-25 15:38:32 -0700

    Added new page: JS Time & Date

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -17,7 +17,7 @@ function scrollFunction() {
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+mybutton.addEventListener("click", backToTop());
 
 function backToTop() {
   document.body.scrollTop = 0;
